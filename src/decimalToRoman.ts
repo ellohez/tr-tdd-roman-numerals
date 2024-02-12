ba5ded65a6b21718c5ccd0174b5f9c5540c7e0ea
Helen Pickavance <ellohez@gmail.com>
2024-02-12 18:59:47 +0000

    fix: refactor and tests passing for numbers up to 10

--- src/decimalToRoman.ts
+++ src/decimalToRoman.ts
@@ -5,18 +5,35 @@ import {
 
 const numeralMap = new Map<number, string>([
   [1, "I"],
+  [2, "II"],
+  [3, "III"],
   [4, "IV"],
   [5, "V"],
+  [6, "VI"],
+  [7, "VII"],
+  [8, "VIII"],
   [9, "IX"],
   [10, "X"],
+  [20, "XX"],
+  [30, "XXX"],
   [40, "XL"],
   [50, "L"],
+  [60, "LX"],
+  [70, "LXX"],
+  [80, "LXXX"],
   [90, "XC"],
   [100, "C"],
+  [200, "CC"],
+  [300, "CCC"],
   [400, "CD"],
   [500, "D"],
+  [600, "DC"],
+  [700, "DCC"],
+  [800, "DCCC"],
   [900, "CM"],
   [1000, "M"],
+  [2000, "MM"],
+  [3000, "MMM"],
 ]);
 
 export const convertDecimalToRoman = (decimal: number): string => {
@@ -33,31 +50,17 @@ export const convertDecimalToRoman = (decimal: number): string => {
 
   let romanNumerals = "";
   const arrayOfNumbers = splitDecimalIntoDigits(decimal);
-  romanNumerals = convertNumberOfOnes(arrayOfNumbers[3]);
+  // Reverse the array, so that we deal with the
+  // ones, tens, hundreds and thousands in that order.
+  arrayOfNumbers.reverse();
+  const onesInNumerals = numeralMap.get(arrayOfNumbers[0]);
+  // tensInNumerals = arrayOfNumbers.length > 1 && arrayOfNumbers[1] * 10 !== 0 ? convert array number : "";
+  // hundredsInNumerals = arrayOfNumbers.length > 2 && arrayOfNumbers[2] * 100 !== 0 ? convert array number : "";
+  // thousandsInNumerals = arrayOfNumbers.length > 3 && arrayOfNumbers[3] * 1000 !== 0 ? convert array number : "";
+
+  romanNumerals += onesInNumerals;
 
   // TODO: Convert and return - this is just for testing.
   return romanNumerals !== "" ? romanNumerals : decimal.toString();
 };
 
-const convertNumberOfOnes = (decimalOnes: number): string => {
-  let romanNumerals = "";
-  // Convert number of 1s to Roman Numerals
-  const simpleNumeral = numeralMap.get(decimalOnes);
-  if (simpleNumeral !== undefined) {
-    romanNumerals += simpleNumeral;
-  }
-  // Simple code to deal with converting values < 10
-  // ignoring 5/V and 10/X as we have already dealt with them.
-  else if (decimalOnes < 4) {
-    for (let i = 1; i <= 3; i++) {
-      romanNumerals += numeralMap.get(1);
-    }
-  } else if (decimalOnes > 5 && decimalOnes < 9) {
-    romanNumerals += numeralMap.get(5);
-    for (let i = 6; i < 9; i++) {
-      romanNumerals += numeralMap.get(1);
-    }
-  }
-
-  return romanNumerals;
-};
